9f020a72cf9d9fe1af479b3a5b73e36a9df930cf
UsefulSan <usefulsan@yandex.ru>
2022-12-21 14:24:40 +0300

    create models for users and ads

--- skymarket/ads/models.py
+++ skymarket/ads/models.py
@@ -1,12 +1,25 @@
+import datetime
+
 from django.conf import settings
 from django.db import models
 
+from users.models import User
+
+
 
 class Ad(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    title = models.CharField(max_length=255)
+    price = models.PositiveSmallIntegerField()
+    description = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+
+    class Meta:
+        ordering = ['created_at']
 
 
 class Comment(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    text = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
--- skymarket/users/models.py
+++ skymarket/users/models.py
@@ -1,16 +1,21 @@
+from enum import Enum
+
 from django.contrib.auth.models import AbstractBaseUser, AbstractUser
 from django.db import models
-from users.managers import UserManager
+# from users.managers import UserManager
 from phonenumber_field.modelfields import PhoneNumberField
 from django.utils.translation import gettext_lazy as _
 
 
-class UserRoles:
-    # TODO закончите enum-класс для пользователя
-    pass
+class UserRoles(Enum):
+    admin = "admin"
+    user = "user"
 
 
 class User(AbstractBaseUser):
-    # TODO переопределение пользователя.
-    # TODO подробности также можно поискать в рекоммендациях к проекту
-    pass
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    phone = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(max_length=40, unique=True)
+    role = models.CharField(max_length=40, choices=[UserRoles])
+    image = models.ImageField(upload_to="image/", null=True)
